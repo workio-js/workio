123556996bf223e74c936d14cfb37409ea6f2c1d
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-10-19 18:40:51 +0900

    wip

--- docs/docs/.vitepress/theme/custom.vue
+++ docs/docs/.vitepress/theme/custom.vue
@@ -7,7 +7,7 @@ import DefaultTheme from 'vitepress/theme';
     <template #home-hero-image
       ><iframe
         id="sb-preview"
-        src="https://stackblitz.com/edit/web-platform-hceprw?embed=1&file=hello.js&hideExplorer=1&hideNavigation=1"
+        src="https://stackblitz.com/edit/web-platform-hceprw?file=hello.ts&hideExplorer=1&hideNavigation=1"
         style="visibility:hidden;"
         onload="this.style.visibility = 'visible';"
       ></iframe
